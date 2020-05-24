1f0de6d8d78d528e3d0e4acd138f06dbe754c1da
jocrock <jocrock.cga@gmail.com>
2020-05-24 18:20:07 -0400

    se agrego el request ArchivoAdjuntoComunicado y se agrago las reglas de validacion

--- app/Models/ArchivoAdjuntoComunicado.php
+++ app/Models/ArchivoAdjuntoComunicado.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class ArchivoAdjuntoComunicado extends Model
 {
-    //
+    public $table = "archivoadjuntocomunicado";
+    protected $fillable = [
+        'URL',
+        'EXTENCION',
+        'idCOMUNICADO'
+
+    ];
+    public $timestamsp = false;
 }
