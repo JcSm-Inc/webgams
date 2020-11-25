fecadbc577f07dedee6cd5f3bcddd8b45a9e7fe3
unknown <sarai45mamani@gmail.com>
2020-11-24 23:49:17 -0400

    se mando las semillas de productos

--- database/seeds/ProductosSeeder.php
+++ database/seeds/ProductosSeeder.php
@@ -122,5 +122,185 @@ class ProductosSeeder extends Seeder
             'FOTO' => 'storage/img/productos/ME12.jpg',
             'STOCK' => '0'
         ]);
-    }
+        Productos::create([
+            'CODPROD' => 'ME13',
+            'NOMBRE' => 'HOJAS BOND',
+            'DESCRIPCION' => 'HOJAS BOND TAMAÑO CARTA',
+            'TIPO' => 'MATERIAL DE ESCRITORIO',
+            'UNIDAD' => 'PAQUETE',
+            'FOTO' => 'storage/img/productos/ME13.jpg',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME14',
+            'NOMBRE' => 'HOJAS BOND',
+            'DESCRIPCION' => 'HOJAS BOND TAMAÑO OFICIO',
+            'TIPO' => 'MATERIAL DE ESCRITORIO',
+            'UNIDAD' => 'PAQUETE',
+            'FOTO' => 'storage/img/productos/ME14.JFIF',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME15',
+            'NOMBRE' => 'HOJAS BOND COLOR',
+            'DESCRIPCION' => 'HOJAS BOND TAMAÑO CARTA',
+            'TIPO' => 'MATERIAL DE ESCRITORIO',
+            'UNIDAD' => 'PAQUETE',
+            'FOTO' => 'storage/img/productos/ME15.JPG',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME16',
+            'NOMBRE' => 'BOLIGRAFO AZUL',
+            'DESCRIPCION' => 'BOLIGRAFO AZUL',
+            'TIPO' => 'MATERIAL DE ESCRITORIO',
+            'UNIDAD' => 'UNIDAD',
+            'FOTO' => 'storage/img/productos/ME16.JFIF',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME17',
+            'NOMBRE' => 'BOLIGRAFO ROJO',
+            'DESCRIPCION' => 'BOLIGRAFO ROJO',
+            'TIPO' => 'MATERIAL DE ESCRITORIO',
+            'UNIDAD' => 'UNIDAD',
+            'FOTO' => 'storage/img/productos/ME17.JPG',
+            'STOCK' => '0'
+        ]);
+
+        Productos::create([
+            'CODPROD' => 'ME18',
+            'NOMBRE' => 'BOLIGRAFO NEGRO',
+            'DESCRIPCION' => 'BOLIGRAFO NEGRO',
+            'TIPO' => 'MATERIAL DE ESCRITORIO',
+            'UNIDAD' => 'UNIDAD',
+            'FOTO' => 'storage/img/productos/ME18.JFIF',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME19',
+            'NOMBRE' => 'LAPIZ NEGRO',
+            'DESCRIPCION' => 'LAPIZ NEGRO',
+            'TIPO' => 'MATERIAL DE ESCRITORIO',
+            'UNIDAD' => 'UNIDAD',
+            'FOTO' => 'storage/img/productos/ME19.JFIF',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME20',
+            'NOMBRE' => 'LAPIZ ROJO',
+            'DESCRIPCION' => 'LAPIZ ROJO',
+            'TIPO' => 'MATERIAL DE ESCRITORIO',
+            'UNIDAD' => 'UNIDAD',
+            'FOTO' => 'storage/img/productos/ME19.JFIF',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME21',
+            'NOMBRE' => 'CORRECTOR DE CINTA',
+            'DESCRIPCION' => 'CORRECTOR DE CINTA',
+            'TIPO' => 'MATERIAL DE ESCRITORIO',
+            'UNIDAD' => 'UNIDAD',
+            'FOTO' => 'storage/img/productos/ME20.JFIF',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME22',
+            'NOMBRE' => 'POSIT DIFERENTES COLORES',
+            'DESCRIPCION' => 'POSIT DIFERENTES COLORES',
+            'TIPO' => 'MATERIAL DE ESCRITORIO',
+            'UNIDAD' => 'UNIDAD',
+            'FOTO' => 'storage/img/productos/ME22.JFIF',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME23',
+            'NOMBRE' => 'PEGAMENTO ISOCOLA',
+            'DESCRIPCION' => 'PEGAMENTO ISOCOLA MEDIANO',
+            'TIPO' => 'MATERIAL DE ESCRITORIO',
+            'UNIDAD' => 'UNIDAD',
+            'FOTO' => 'storage/img/productos/ME23.JPG',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME24',
+            'NOMBRE' => 'PEGAMENTO UHU',
+            'DESCRIPCION' => 'PEGAMENTO UHU',
+            'TIPO' => 'MATERIAL DE ESCRITORIO',
+            'UNIDAD' => 'UNIDAD',
+            'FOTO' => 'storage/img/productos/ME24.PNG',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME25',
+            'NOMBRE' => 'FOLDER AMARILLO',
+            'DESCRIPCION' => 'FOLDER AMARILLO TAMAÑO OFICIO',
+            'TIPO' => 'MATERIAL DE ESCRITORIO',
+            'UNIDAD' => 'UNIDAD',
+            'FOTO' => 'storage/img/productos/ME16.JFIF',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME26',
+            'NOMBRE' => 'CUADERNO ESPERIRAL OFICIO',
+            'DESCRIPCION' => 'CUADERNO ESPERIRAL TAMAÑO OFICIO',
+            'TIPO' => 'MATERIAL DE ESCRITORIO',
+            'UNIDAD' => 'UNIDAD',
+            'FOTO' => 'storage/img/productos/ME26.JPG',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME27',
+            'NOMBRE' => 'PESTAÑAS PLASTICAS',
+            'DESCRIPCION' => 'PESTAÑAS PLASTICAS',
+            'TIPO' => 'MATERIAL DE ESCRITORIO',
+            'UNIDAD' => 'UNIDAD',
+            'FOTO' => 'storage/img/productos/ME27.JFIF',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME28',
+            'NOMBRE' => 'AMBIENTADOR',
+            'DESCRIPCION' => 'AMBIENTADOR DIFERENTES AROMAS',
+            'TIPO' => 'MATERIAL DE LIMPIEZA',
+            'UNIDAD' => 'UNIDAD',
+            'FOTO' => 'storage/img/productos/ME28.JPG',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME29',
+            'NOMBRE' => 'FRANELA 30*30',
+            'DESCRIPCION' => 'FRANELA 30*30',
+            'TIPO' => 'MATERIAL DE LIMPIEZA',
+            'UNIDAD' => 'UNIDAD',
+            'FOTO' => 'storage/img/productos/ME29.JFIF',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME30',
+            'NOMBRE' => 'SANITIZADOR DE MANOS',
+            'DESCRIPCION' => 'SANITIZADOR DE MANOS',
+            'TIPO' => 'MATERIAL DE LIMPIEZA',
+            'UNIDAD' => 'UNIDAD',
+            'FOTO' => 'storage/img/productos30.JPG',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME31',
+            'NOMBRE' => 'OLA MULTIUSO PARA COMPUTADOR',
+            'DESCRIPCION' => 'SANITVOLA MULTIUSO PARA COMPUTADOR',
+            'TIPO' => 'MATERIAL DE LIMPIEZA',
+            'UNIDAD' => 'UNIDAD',
+            'FOTO' => 'storage/img/productos/ME31.JPG',
+            'STOCK' => '0'
+        ]);
+        Productos::create([
+            'CODPROD' => 'ME32',
+            'NOMBRE' => 'OVERON',
+            'DESCRIPCION' => 'OBEROL',
+            'TIPO' => 'ACCESORIOS',
+            'UNIDAD' => 'UNIDAD',
+            'FOTO' => 'storage/img/productos/ME32.JPG',
+            'STOCK' => '0'
+        ]);
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,6 +12,45 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        factory(User::class,500)->create();
+        //factory(User::class,500)->create();
+        User::create([
+            'nick' => 'OSCAR',
+            'email' => 'OSCAR@GMAIL.COM',
+            'email_verified_at' => 'OSCAR@GMAIL.COM',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'CI' => '4061782',
+            'NOMBRES' => 'OSCAR',
+            'APELLIDOS' => 'FLORES ADRIAN',
+            'FECHANACIMIENTO' => '1968/12/05',
+            'FOTO' => 'storage/img/user/U1.JPG',
+            'GENERO' => 'MASCULINO',
+            'TIPO' => 'USUARIO EXTERNO',
+        ])
+        User::create([
+            'nick' => 'FREDDY',
+            'email' => 'FREDY@GMAIL.COM',
+            'email_verified_at' => 'FREDY@GMAIL.COM',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'CI' => '3096102',
+            'NOMBRES' => 'FREDDY',
+            'APELLIDOS' => 'SUARES CAMARGO',
+            'FECHANACIMIENTO' => '1968/11/05',
+            'FOTO' => 'storage/img/user/U2.JPG',
+            'GENERO' => 'MASCULINO',
+            'TIPO' => 'USUARIO EXTERNO',
+        ])
+        User::create([
+            'nick' => 'LUIS',
+            'email' => 'LUIS@GMAIL.COM',
+            'email_verified_at' => 'LUIS@GMAIL.COM',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'CI' => '3096102',
+            'NOMBRES' => 'FREDDY',
+            'APELLIDOS' => 'SUARES CAMARGO',
+            'FECHANACIMIENTO' => '1968/11/05',
+            'FOTO' => 'storage/img/users/U2.JPG',
+            'GENERO' => 'MASCULINO',
+            'TIPO' => 'USUARIO EXTERNO',
+        ])
     }
 }
