09557b313cb10129833296b7e26faf1693414d22
unknown <sarai45mamani@gmail.com>
2020-11-29 15:28:01 -0400

    se mando las semillas de productosusuarios

--- database/seeds/PersonalDePlantaSeeder.php
+++ database/seeds/PersonalDePlantaSeeder.php
@@ -12,6 +12,50 @@ class PersonalDePlantaSeeder extends Seeder
      */
     public function run()
     {
-        factory(Personal_de_Planta::class, 20)->create();
+       // factory(Personal_de_Planta::class, 20)->create();
+       User::create([
+       'CARGO' => 'ALCALDE MUNICIPAL',
+       'idUSUARIO' => '1',
+       ]);
+       User::create([
+        'CARGO' => 'SECRETARIO MINICIPAL ADMISTRATIVO Y DE DESARROLLO INSTITUCIONAL',
+        'idUSUARIO' => '2',
+        ]);
+        User::create([
+            'CARGO' => 'RESPONSABLE DE ALMACENES',
+            'idUSUARIO' => '3',
+            ]);
+            User::create([
+                'CARGO' => 'SECRETARIO MINICIPAL PLANIFICASION Y OBRAS PUBLICAS',
+                'idUSUARIO' => '4',
+                ]);
+                User::create([
+                    'CARGO' => 'DIRECTOR ORDENAMINETO TERRITORIAL',
+                    'idUSUARIO' => '5',
+                    ]);
+                    User::create([
+                        'CARGO' => 'DIRECTOR DE RECURSOS HIDRICOS Y AGROPECUARIA',
+                        'idUSUARIO' => '6',
+                        ]);
+                        User::create([
+                            'CARGO' => 'DIRECTOR DE MAQUINARIA TRAFICO Y VIALIDAD',
+                            'idUSUARIO' => '7',
+                            ]);
+                            User::create([
+                                'CARGO' => 'DIRECTOR DE DESARROLLO HUMANO',
+                                'idUSUARIO' => '8',
+                                ]);
+                                User::create([
+                                    'CARGO' => 'DIRECTOR ADMINISTRATIVO Y FINANCIERA',
+                                    'idUSUARIO' => '9',
+                                    ]);
+                                    User::create([
+                                        'CARGO' => 'DIRECTOR  DE ASUNTOS JURIDICOS',
+                                        'idUSUARIO' => '10',
+                                        ]);
+                                        User::create([
+                                            'CARGO' => 'CONTADORA',
+                                            'idUSUARIO' => '11',
+                                            ]);
     }
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
     {
         //factory(User::class,500)->create();
         User::create([
-            'nick' => 'OSCAR',
+            'nick' => 'OSCAR_FA',
             'email' => 'OSCAR@GMAIL.COM',
             'email_verified_at' => 'OSCAR@GMAIL.COM',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
@@ -24,10 +24,10 @@ class UserSeeder extends Seeder
             'FECHANACIMIENTO' => '1968/12/05',
             'FOTO' => 'storage/img/user/U1.JPG',
             'GENERO' => 'MASCULINO',
-            'TIPO' => 'USUARIO EXTERNO',
-        ])
+            'TIPO' => 'USUARIO INTERNO'
+        ]);
         User::create([
-            'nick' => 'FREDDY',
+            'nick' => 'FREDDY_SC',
             'email' => 'FREDY@GMAIL.COM',
             'email_verified_at' => 'FREDY@GMAIL.COM',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
@@ -37,20 +37,150 @@ class UserSeeder extends Seeder
             'FECHANACIMIENTO' => '1968/11/05',
             'FOTO' => 'storage/img/user/U2.JPG',
             'GENERO' => 'MASCULINO',
-            'TIPO' => 'USUARIO EXTERNO',
-        ])
+            'TIPO' => 'USUARIO INTERNO'
+        ]);
         User::create([
-            'nick' => 'LUIS',
+            'nick' => 'GUIDO_CB',
+            'email' => 'GUIDO@GMAIL.COM',
+            'email_verified_at' => 'GUIDO@GMAIL.COM',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'CI' => '3107693',
+            'NOMBRES' => 'GUIDO',
+            'APELLIDOS' => 'CHOQUE BLANCO',
+            'FECHANACIMIENTO' => '1990/12/28',
+            'FOTO' => 'storage/img/users/U9.JPG',
+            'GENERO' => 'FEMENINO',
+            'TIPO' => 'USUARIO INTERNO'
+        ]);
+        User::create([
+            'nick' => 'LUIS_MA',
             'email' => 'LUIS@GMAIL.COM',
             'email_verified_at' => 'LUIS@GMAIL.COM',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'CI' => '3096102',
-            'NOMBRES' => 'FREDDY',
-            'APELLIDOS' => 'SUARES CAMARGO',
+            'NOMBRES' => 'lUIS ALBERTO',
+            'APELLIDOS' => 'MARTINEZ COLLARANA',
             'FECHANACIMIENTO' => '1968/11/05',
-            'FOTO' => 'storage/img/users/U2.JPG',
+            'FOTO' => 'storage/img/users/U3.JPG',
             'GENERO' => 'MASCULINO',
-            'TIPO' => 'USUARIO EXTERNO',
-        ])
+            'TIPO' => 'USUARIO INTERNO'
+        ]);
+        User::create([
+            'nick' => 'CARLOS_CR',
+            'email' => 'CARLOS@GMAIL.COM',
+            'email_verified_at' => 'CARLOS@GMAIL.COM',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'CI' => '3107693',
+            'NOMBRES' => 'CARLOS OSCAR',
+            'APELLIDOS' => 'CALLE ROMERO',
+            'FECHANACIMIENTO' => '1968/11/05',
+            'FOTO' => 'storage/img/users/U4.JPG',
+            'GENERO' => 'MASCULINO',
+            'TIPO' => 'USUARIO INTERNO'
+        ]);
+        User::create([
+            'nick' => 'CARLOS_CA',
+            'email' => 'CARLOSA@GMAIL.COM',
+            'email_verified_at' => 'CARLOSA@GMAIL.COM',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'CI' => '3107693',
+            'NOMBRES' => 'CARLOS ',
+            'APELLIDOS' => 'CONDORI ANTONIO',
+            'FECHANACIMIENTO' => '1968/11/05',
+            'FOTO' => 'storage/img/users/U5.JPG',
+            'GENERO' => 'MASCULINO',
+            'TIPO' => 'USUARIO INTERNO',
+        ]);
+        User::create([
+            'nick' => 'ALBERTO_MJ',
+            'email' => 'ALBERTO@GMAIL.COM',
+            'email_verified_at' => 'ALBERTO@GMAIL.COM',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'CI' => '3107693',
+            'NOMBRES' => 'ALBERTO',
+            'APELLIDOS' => 'MAMANI JANCO',
+            'FECHANACIMIENTO' => '1968/11/05',
+            'FOTO' => 'storage/img/users/U6.JPG',
+            'GENERO' => 'MASCULINO',
+            'TIPO' => 'USUARIO INTERNO',
+        ]);
+        User::create([
+            'nick' => 'RAUL_VA',
+            'email' => 'RAUL@GMAIL.COM',
+            'email_verified_at' => 'RAUL@GMAIL.COM',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'CI' => '3107693',
+            'NOMBRES' => 'RAUL',
+            'APELLIDOS' => 'VALENCIA ALANES',
+            'FECHANACIMIENTO' => '1968/11/05',
+            'FOTO' => 'storage/img/users/U7.JPG',
+            'GENERO' => 'MASCULINO',
+            'TIPO' => 'USUARIO INTERNO',
+        ]);
+        User::create([
+            'nick' => 'VIRGINIA_AA',
+            'email' => 'VIRGINIA@GMAIL.COM',
+            'email_verified_at' => 'VIRGINIA@GMAIL.COM',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'CI' => '3107693',
+            'NOMBRES' => 'VIRGINIA JHENNY',
+            'APELLIDOS' => 'APAZA ALCALA',
+            'FECHANACIMIENTO' => '1968/11/05',
+            'FOTO' => 'storage/img/users/UA1.JPG',
+            'GENERO' => 'FEMENINO',
+            'TIPO' => 'USUARIO INTERNO',
+        ]);
+        User::create([
+            'nick' => 'MARIA_PM',
+            'email' => 'MARIA@GMAIL.COM',
+            'email_verified_at' => 'MARIA@GMAIL.COM',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'CI' => '3107693',
+            'NOMBRES' => 'MARIA LUIZA',
+            'APELLIDOS' => 'PEREZ MERCADO',
+            'FECHANACIMIENTO' => '1968/11/05',
+            'FOTO' => 'storage/img/users/UA2.JPG',
+            'GENERO' => 'FEMENINO',
+            'TIPO' => 'USUARIO INTERNO',
+        ]);
+        User::create([
+            'nick' => 'SARAI_MM',
+            'email' => 'SARAI@GMAIL.COM',
+            'email_verified_at' => 'SARAI@GMAIL.COM',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'CI' => '3107693',
+            'NOMBRES' => 'SARAI ESTHER',
+            'APELLIDOS' => 'MAMANI MARTINEZ',
+            'FECHANACIMIENTO' => '1995/12/28',
+            'FOTO' => 'storage/img/users/UA3.JPEG',
+            'GENERO' => 'FEMENINO',
+            'TIPO' => 'USUARIO INTERNO',
+        ]);
+        User::create([
+            'nick' => 'GABRIELA_MM',
+            'email' => 'GABRIELA_@GMAIL.COM',
+            'email_verified_at' => 'GABRIELA_@GMAIL.COM',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'CI' => '3107693',
+            'NOMBRES' => 'GABRIELA',
+            'APELLIDOS' => 'MAMANI LUNA',
+            'FECHANACIMIENTO' => '1995/12/28',
+            'FOTO' => 'storage/img/users/UA4.JPG',
+            'GENERO' => 'FEMENINO',
+            'TIPO' => 'USUARIO INTERNO',
+        ]);
+        User::create([
+            'nick' => 'GUIDO_CB',
+            'email' => 'GUIDO@GMAIL.COM',
+            'email_verified_at' => 'GUIDO@GMAIL.COM',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'CI' => '3107693',
+            'NOMBRES' => 'GUIDO',
+            'APELLIDOS' => 'CHOQUE BLANCO',
+            'FECHANACIMIENTO' => '1990/12/28',
+            'FOTO' => 'storage/img/users/U9.JPG',
+            'GENERO' => 'FEMENINO',
+            'TIPO' => 'USUARIO INTERNO',
+        ]);
     }
 }
